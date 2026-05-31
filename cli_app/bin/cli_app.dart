92202a5acfe11bb2ad19e23f5e717bbfd1d792b9
Egor <egassk@gmail.com>
2026-05-31 18:43:10 +0300

    2 урок: сделал CLI интерактивным — программа различает команды version, help и search, а при поиске сама запрашивает тему, если её не ввели

--- cli_app/bin/cli_app.dart
+++ cli_app/bin/cli_app.dart
@@ -1,4 +1,37 @@
-void main() {
-  print('Hello, Dart!');
-  print('Добро пожаловать в обучающий курс!');
+import 'dart:io';
+
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
